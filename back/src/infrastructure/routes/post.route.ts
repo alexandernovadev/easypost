6d7a6dfd32ad9a9c6ccaf6e5199ad5515e39daee
alexandernovadev <alexsk88.dev@gmail.com>
2023-08-17 13:22:32 -0500

    Post validation done

--- back/src/infrastructure/routes/post.route.ts
+++ back/src/infrastructure/routes/post.route.ts
@@ -2,14 +2,15 @@ import { Router } from 'express'
 import * as PostController from '../controllers/post.controller'
 import { validatePost } from '../middlewares/dataValidate/PostValidate'
 import { validateObjectId } from '../middlewares/validateObjectId'
+import authenticate from '../middlewares/auth.middleware'
 // Importa cualquier middleware que necesites, como uno para validar datos del post.
 
 const router = Router()
 
-router.get('/', PostController.getAllPosts)
-router.post('/', validatePost, PostController.createPost)
-router.get('/:id', validateObjectId, PostController.getPost)
-router.put('/:id', validateObjectId, PostController.updatePost)
-router.delete('/:id', validateObjectId, PostController.deletePost)
+router.get('/', authenticate, PostController.getAllPosts)
+router.post('/', authenticate, validatePost, PostController.createPost)
+router.get('/:id', authenticate, validateObjectId, PostController.getPost)
+router.put('/:id', authenticate, validateObjectId, PostController.updatePost)
+router.delete('/:id', authenticate, validateObjectId, PostController.deletePost)
 
 export default router
