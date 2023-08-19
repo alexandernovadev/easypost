1e6c9a8d100fcef41e493ef4734bb046759e4b67
alexandernovadev <alexsk88.dev@gmail.com>
2023-08-19 16:24:25 -0500

    Menu finished

--- front/src/components/atomic/molecules/navbar/Nabvar.tsx
+++ front/src/components/atomic/molecules/navbar/Nabvar.tsx
@@ -1,17 +1,74 @@
-import React from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { NavbarStyle } from './navbar.style'
 import loogMain from '../../../../assets/logoMain.svg'
 import menu from '../../../../assets/menuhamburger.svg'
 import { Typography } from '../../atoms/typography/Typography'
 import { themeLight } from '../../../../themes/themeLight'
+import { Link, useNavigate } from 'react-router-dom'
+import { CreateIcon } from '../../../../assets/Components/CreateIcon'
 
 export const Nabvar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+  const menuRef = useRef<HTMLDivElement | null>(null)
+
+  const navigate = useNavigate()
+  const toogleMenu = () => {
+    setIsMenuOpen((prev) => !prev)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
+  const goToHome = () => navigate('/allposthome')
+  
   return (
     <NavbarStyle>
-      <img src={loogMain} alt="" />
-      <Typography  variant='subtitle' text='Easy Media' color={themeLight.colors.textContrast}  />
-      <img src={menu} alt="" />
+      <div className="menuLogo" onClick={goToHome}>
+        <img src={loogMain} alt="Logo" />
+        <Typography
+          variant="subtitle"
+          text="Easy Media"
+          color={themeLight.colors.textContrast}
+        />
+      </div>
+      <div className="menuH">
+        <img src={menu} alt="Menu" onClick={toogleMenu} />
+        {isMenuOpen && (
+          <div className="menuOptions open" ref={menuRef}>
+            <div className="titleSesion">Camilo Cifuentes</div>
 
+            <Link to="/createpost" className="optionMenu">
+              <CreateIcon />
+              Create Publication
+            </Link>
+
+            <Link to="/post" className="optionMenu">
+              <CreateIcon />
+              My Publications
+            </Link>
+
+            <Link to="/allposthome" className="optionMenu">
+              <CreateIcon />
+              See All Publications
+            </Link>
+
+            <Link to="/post" className="optionMenu">
+              <CreateIcon />
+              Logout
+            </Link>
+          </div>
+        )}
+      </div>
     </NavbarStyle>
   )
 }
--- front/src/components/atomic/molecules/navbar/navbar.style.ts
+++ front/src/components/atomic/molecules/navbar/navbar.style.ts
@@ -4,8 +4,80 @@ export const NavbarStyle = styled.nav`
   width: 100%;
   height: 40px;
   background: ${({ theme }) => theme.colors.white};
-
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
+  padding: 0px 22px;
+
+  .menuLogo{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+    cursor: pointer;
+  }
+
+  .menuH {
+    position: relative; // Make it relative so the dropdown is positioned based on this
+
+    img {
+      cursor: pointer; // Show that the image is clickable
+    }
+
+    .menuOptions {
+      display: none; // Initially hide the menu
+      position: absolute; // Absolute position to show it just below the hamburger icon
+      top: 100%; // Position it just below the parent div
+      right: 0; // Align it to the right
+      background-color: ${({ theme }) => theme.colors.white};
+      border: 1px solid #ccc;
+      border-radius: 12px;
+      z-index: 1; // To show it above other elements
+      min-width: 145px;
+      display: flex;
+      flex-direction: column;
+      overflow: auto;
+
+      .titleSesion {
+        color: #0e2248;
+        font-size: 15px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+        border-bottom: 1px solid #dddada;
+        padding-bottom: 4px;
+        margin-bottom: 6px;
+        text-align: center;
+        padding: 10px;
+      }
+
+      .optionMenu {
+        padding: 10px;
+
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        gap: 7px;
+        padding-top: 4px;
+
+      
+          cursor: pointer;
+          color: #0e2248;
+          font-family: Lato;
+          font-size: 13px;
+          font-style: normal;
+          font-weight: 300;
+          line-height: normal;
+          text-decoration: none;
+        
+        &:hover {
+          background-color: #DDDADA;
+        }
+      }
+      &.open {
+        // Add a class to show the menu
+        display: block;
+      }
+    }
+  }
 `
