0e969b1b1a2b6e827e7474bcd06ec699fccdc9ce
alexandernovadev <alexsk88.dev@gmail.com>
2023-08-21 23:50:43 -0500

    move piplines

---
+++ backPipeLine.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Crear un directorio temporal
+tmpdir=$(mktemp -d)
+
+# Copiar todo desde 'front' al directorio temporal
+cp -r back/* "$tmpdir"
+
+# Cambiar a la rama 'front'
+if git checkout back; then
+    # Copiar los archivos desde el directorio temporal a la raíz
+    cp -r "$tmpdir"/* .
+
+    # Borrar las carpetas 'back', 'front' y 'node_modules'
+    rm -rf back front node_modules
+
+    # Agregar los cambios a Git
+    git add .
+
+    # Hacer una confirmación con un mensaje
+    git commit -m "Actualización de la versión de 'Back 1.2.0'"
+
+    # Empujar los cambios a la rama 'front' en el repositorio remoto
+    git push origin back
+else
+    echo "No se pudo cambiar a la rama 'back'. Operación abortada."
+fi
+
+# Eliminar el directorio temporal
+rm -rf "$tmpdir"
