c2e3734e9b311b0d97b46a74b4b577da3e7faf89
alexandernovadev <alexsk88.dev@gmail.com>
2023-08-22 01:27:23 -0500

    new pipe 2

--- newpipe.sh
+++ newpipe.sh
@@ -25,12 +25,9 @@ default_version="1.0.0"
 # Obtener la versión como argumento o usar el valor por defecto
 version="${1:-$default_version}"
 
-# Nombre de la rama 'front'
+# Ramas 'front'
 front_branch="front"
-
-# Nombre de la rama 'backend'
 backend_branch="backend"
-
 # Incrementar el número de versión de revisión
 increment_version "$current_version"
 
@@ -43,27 +40,13 @@ tmpdir=$(mktemp -d)
 # Copiar todo desde 'backend' al directorio temporal
 cp -r backend/* "$tmpdir"
 
-# Cambiar a la rama 'front'
 if git checkout "$front_branch"; then
-    # Copiar los archivos desde el directorio temporal a la raíz
     cp -r "$tmpdir"/* .
-
-    # Borrar las carpetas 'backend', 'frontend' y 'node_modules'
-    rm -rf backend frontend node_modules README.md
-
-    # Agregar los cambios a Git
+    rm -rf back front node_modules README.md
     git add .
-
-    # Hacer una confirmación con un mensaje
     git commit -m "Actualización de la versión a '$new_version'"
-
-    # Empujar los cambios a la rama 'front' en el repositorio remoto
     git push origin "$front_branch"
-
-    # Volver a la rama 'main'
     git checkout main
-
-    # Mostrar mensaje de actualización exitosa
     echo "¡Actualización y subida correctas a la rama '$front_branch' con nueva versión '$new_version'!"
 else
     echo "No se pudo cambiar a la rama '$front_branch'. Operación abortada."
