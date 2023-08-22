dbed775a6481f886d8cae160646b9379b9524b70
alexandernovadev <alexsk88.dev@gmail.com>
2023-08-22 00:22:12 -0500

    Actualización de la versión de 'front 1.2.4'

---
+++ src/components/atomic/molecules/emtyposts/EmptyPost.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const EmptyPostStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  width: 100%;
+  height: 60vh;
+  justify-content: center;
+  align-items: center;
+`
